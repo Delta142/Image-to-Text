71896de4badf6e62092576b3afe47b9de17eae84
LC-Person <98434562+LC-Person@users.noreply.github.com>
2023-11-08 16:26:31 -0500

    Add files via upload

--- Tools/ImageToTextWithTesseract.py
+++ Tools/ImageToTextWithTesseract.py
@@ -3,6 +3,7 @@ import pytesseract
 #Image processing
 from PIL import Image
 import os
+import time
 
 def getImageText(pathUsed="./Train_data/text/sampletext.png"):
     #Open image
@@ -32,11 +33,19 @@ def getImageText(pathUsed="./Train_data/text/sampletext.png"):
     '''
     return Text
 
-def saveImageText(pathUsed="TextSaved.txt", textGenerated="Information Lost..."):
-    file=open(pathUsed,"a")
+def saveImageText(savedPath="Scanned_History.txt", pathUsed="NotFound", textGenerated="Information Lost..."):
+    file=open(savedPath,"a")
+    file.write(f'Time Scanned: {time.ctime()}\nFile Scanned: [{pathUsed}]\n')
+    file.write(f'-----------------------------------------------------------------\n')
     file.write(textGenerated)
+    file.write(f'-----------------------------------------------------------------\n')
     file.write("\n")
     file.close()
 
 def saveImageTextSingle(pathUsed="/Null/NoName.png", textGenerated="Picture Was Empty"):
-    pass
+    Pathsplit = pathUsed.split('/')
+    FileName = (Pathsplit[-1]).split('.')
+
+    file=open(FileName[0],"w")
+    file.write(textGenerated)
+    file.close()
