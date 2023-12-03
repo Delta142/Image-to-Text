08ea93fa74fb275774b0d96e787cfcb5cd74ee09
Moneboi <98432686+Moneboi@users.noreply.github.com>
2023-12-03 18:55:40 -0500

    Update ImageScannerTest.py

--- ImageScannerTest.py
+++ ImageScannerTest.py
@@ -8,6 +8,9 @@ from os import listdir, path
 import ImageToTextWithTesseract as ImTeTs
 #For: Text to speech
 import pyttsx3
+#For: Translate to different Language
+#import translateText as trText
+from translate import Translator
 
 #be careful of the Globals:
 #download TKInter
@@ -25,7 +28,7 @@ import pyttsx3
 # !Select Language
 # !Select Output Format (Download)
 
-#
+# ...
 DEV = False
 WORK_COLUMN = 1
 IMAGE_ROW = 1
@@ -33,9 +36,116 @@ IMAGE_ROW = 1
 #colors of interface
 BK_G_DEF = '#36454F' #Originally "lightblue"
 BUTTN_COLOR = "cadetblue"
-TITLE_COLOR = "Black"
 LABEL_COLOR = "cyan" #Originally "darkblue"
-WARNING_COLOR = BK_G_DEF
+languages = [
+    ("Afrikaans", "af"),
+    ("Albanian", "sq"),
+    ("Amharic", "am"),
+    ("Arabic", "ar"),
+    ("Armenian", "hy"),
+    ("Azerbaijani", "az"),
+    ("Basque", "eu"),
+    ("Belarusian", "be"),
+    ("Bengali", "bn"),
+    ("Bosnian", "bs"),
+    ("Bulgarian", "bg"),
+    ("Catalan", "ca"),
+    ("Cebuano", "ceb"),
+    ("Chinese (Simplified)", "zh-CN"),
+    ("Chinese (Traditional)", "zh-TW"),
+    ("Corsican", "co"),
+    ("Croatian", "hr"),
+    ("Czech", "cs"),
+    ("Danish", "da"),
+    ("Dutch", "nl"),
+    ("English", "en"),
+    ("Esperanto", "eo"),
+    ("Estonian", "et"),
+    ("Finnish", "fi"),
+    ("French", "fr"),
+    ("Frisian", "fy"),
+    ("Galician", "gl"),
+    ("Georgian", "ka"),
+    ("German", "de"),
+    ("Greek", "el"),
+    ("Gujarati", "gu"),
+    ("Haitian Creole", "ht"),
+    ("Hausa", "ha"),
+    ("Hawaiian", "haw"),
+    ("Hebrew", "he"),
+    ("Hindi", "hi"),
+    ("Hmong", "hmn"),
+    ("Hungarian", "hu"),
+    ("Icelandic", "is"),
+    ("Igbo", "ig"),
+    ("Indonesian", "id"),
+    ("Irish", "ga"),
+    ("Italian", "it"),
+    ("Japanese", "ja"),
+    ("Javanese", "jv"),
+    ("Kannada", "kn"),
+    ("Kazakh", "kk"),
+    ("Khmer", "km"),
+    ("Kinyarwanda", "rw"),
+    ("Korean", "ko"),
+    ("Kurdish (Kurmanji)", "ku"),
+    ("Kyrgyz", "ky"),
+    ("Lao", "lo"),
+    ("Latin", "la"),
+    ("Latvian", "lv"),
+    ("Lithuanian", "lt"),
+    ("Luxembourgish", "lb"),
+    ("Macedonian", "mk"),
+    ("Malagasy", "mg"),
+    ("Malay", "ms"),
+    ("Malayalam", "ml"),
+    ("Maltese", "mt"),
+    ("Maori", "mi"),
+    ("Marathi", "mr"),
+    ("Mongolian", "mn"),
+    ("Myanmar (Burmese)", "my"),
+    ("Nepali", "ne"),
+    ("Norwegian", "no"),
+    ("Odia (Oriya)", "or"),
+    ("Pashto", "ps"),
+    ("Persian", "fa"),
+    ("Polish", "pl"),
+    ("Portuguese", "pt"),
+    ("Punjabi", "pa"),
+    ("Romanian", "ro"),
+    ("Russian", "ru"),
+    ("Samoan", "sm"),
+    ("Scots Gaelic", "gd"),
+    ("Serbian", "sr"),
+    ("Sesotho", "st"),
+    ("Shona", "sn"),
+    ("Sindhi", "sd"),
+    ("Sinhala", "si"),
+    ("Slovak", "sk"),
+    ("Slovenian", "sl"),
+    ("Somali", "so"),
+    ("Spanish", "es"),
+    ("Sundanese", "su"),
+    ("Swahili", "sw"),
+    ("Swedish", "sv"),
+    ("Tajik", "tg"),
+    ("Tamil", "ta"),
+    ("Tatar", "tt"),
+    ("Telugu", "te"),
+    ("Thai", "th"),
+    ("Turkish", "tr"),
+    ("Turkmen", "tk"),
+    ("Ukrainian", "uk"),
+    ("Urdu", "ur"),
+    ("Uyghur", "ug"),
+    ("Uzbek", "uz"),
+    ("Vietnamese", "vi"),
+    ("Welsh", "cy"),
+    ("Xhosa", "xh"),
+    ("Yiddish", "yi"),
+    ("Yoruba", "yo"),
+    ("Zulu", "zu")
+]
 
 #create window
 main_window = Tk()
@@ -52,12 +162,9 @@ for i in range(30): #30 Cells in Width
         if (DEV):
             cell = Frame(main_window, width = 200, height = 35, background= "Blue")
         else:
-            cell = Frame(main_window, width = 200, height = 35, background = BK_G_DEF) # BK_G_DEF
+            cell = Frame(main_window, width = 200, height = 35, background = 'black') # BK_G_DEF
 
         cell.grid(row = i, column = j, padx = 1, pady = 1)
-
-#Header
-Label(main_window, text = "Welcome To Text Reader 9000", background = BK_G_DEF, foreground = "White", font=("Helvetica", 24)).grid(row=1, column=2, columnspan=3, rowspan=1)
 
 #Work Image Container
 CanvasContainer = Canvas(main_window, width = 600, height = 545, background="white", highlightbackground="black", highlightthickness = 1) #Creates widget with set width and height in pixels, and color
@@ -73,7 +180,7 @@ ImageLocation = Entry(main_window, width=32, borderwidth=2) #Creates input box
 ImageLocation.grid(row=IMAGE_ROW+17, column=WORK_COLUMN, columnspan=2, sticky=S) #Places input box on grid
 
 #Image output field
-#Image_Location_Warning = Label(main_window, text="", background=WARNING_COLOR, foreground="red") #Creates warning lable
+#Image_Location_Warning = Label(main_window, text="", background=BK_G_DEF, foreground="red") #Creates warning lable
 #Image_Location_Warning.grid(row=10, column=WORK_COLUMN, columnspan=3) #Places it on grid
 
 ImageOut = Text(main_window, width=67, height=22, borderwidth=2) #Creates textbox for output text 
@@ -166,26 +273,60 @@ def count_letters_and_words():
 def PlaceHolder():
     pass
 
-#Buttons and such
-Label(main_window, text="Image Analysis Output", background = BK_G_DEF, foreground = "White", font=("Helvetica", 20)).grid(row=3,column=4, columnspan=3, rowspan=2)
-Button(main_window, text="Word Scanner", command=WordScanner, background=BUTTN_COLOR, width=20).grid(row=16,column=4, columnspan=2)
-Button(main_window, text="Text to Speech", command=Text2Speech, background=BUTTN_COLOR, width=20).grid(row=17,column=4, columnspan=2)
-Button(main_window, text="Update Language", command=PlaceHolder, background=BUTTN_COLOR, width=20).grid(row=18,column=4, columnspan=2)
-Button(main_window, text="Count Words/Letters", command=count_letters_and_words, background=BUTTN_COLOR, width=20).grid(row=16,column=5, columnspan=2)
-Button(main_window, text="Undefined", command=PlaceHolder, background=BUTTN_COLOR, width=20).grid(row=17,column=5, columnspan=2)
-LanguageOut = Entry(main_window, width=24, borderwidth=2) #Select Language Input Box
-LanguageOut.grid(row=IMAGE_ROW+17, column=5, columnspan=2) #Places input box on grid
+#Performs languages conversion
+def perform_translation():
+        user_lang_input = LanguageOut.get() #Gets entered language choice
+        text_to_translate = ImageOut.get(1.0,"end-1c") #Gets text from read/entered text
+        #print(user_lang_input)
+        #print(text_to_translate)
 
-Button(main_window, text="Save Text", command=SaveIMG, background=BUTTN_COLOR).grid(row=IMAGE_ROW+14,column=WORK_COLUMN+3, sticky=E, padx = 15)
+        #Find the language that matches users input
+        for name, code in languages:
+            if user_lang_input.lower() in name.lower():
+                print(f"Language chosen: {name} and {code}")
+                break
+
+        #If lnaguage entered matches
+        if name:
+                try:
+                    translator = Translator(to_lang=code) #Setup translator to chosen language
+                    translated_text = translator.translate(text_to_translate) #Convert text using new language
+                    ImageOut.delete(1.0, "end") #Clear current text
+                    ImageOut.insert("end", translated_text) #Insert translated text
+                except Exception as e:
+                    print("Translation failed: {e}")
+
+        #If language entered doesnt match
+        else:
+                print("Language not found")
+
+#Buttons/Labels/etc...
+Label(main_window, text = "Welcome To Text Reader 9000", background = BK_G_DEF, foreground = "White", font=("Helvetica", 24)).grid(row=1, column=2, columnspan=3, rowspan=1)
+
 Button(main_window, text="Update Image", command=ChangeImage, width=20, background=BUTTN_COLOR).grid(row=IMAGE_ROW+17,column=WORK_COLUMN-1, columnspan=2, rowspan=1, sticky=S)
 Button(main_window, text="Browse Files", command=browseFiles, width=20, background=BUTTN_COLOR).grid(row=IMAGE_ROW+17,column=WORK_COLUMN+1, columnspan=2, rowspan=1, sticky=S)
 
+Label(main_window, text="Image Analysis Output", background = BK_G_DEF, foreground = "White", font=("Helvetica", 20)).grid(row=3,column=4, columnspan=3, rowspan=2)
+
+Button(main_window, text="Save Text", command=SaveIMG, background=BUTTN_COLOR).grid(row=IMAGE_ROW+14,column=WORK_COLUMN+3, sticky=E, padx = 15)
 WordOut = Label(main_window, text="Word Count: 0", background = BK_G_DEF, foreground = "White", font=("Helvetica", 12))
 WordOut.grid(row=15,column=5, columnspan=1, rowspan=1, sticky=W, padx=10)
-
 CharOut = Label(main_window, text="Character Count: 0", background = BK_G_DEF, foreground = "White", font=("Helvetica", 12))
 CharOut.grid(row=15,column=5, columnspan=2, rowspan=1)
 
+Button(main_window, text="Word Scanner", command=WordScanner, background=BUTTN_COLOR, width=20).grid(row=16,column=4, columnspan=1, sticky=E)
+Button(main_window, text="Count Words/Letters", command=count_letters_and_words, background=BUTTN_COLOR, width=20).grid(row=16,column=5, columnspan=1)
+Button(main_window, text="Text to Speech", command=Text2Speech, background=BUTTN_COLOR, width=20).grid(row=16,column=6, columnspan=1, sticky=W)
 
+LanguageOut = Entry(main_window, width=24, borderwidth=2) #Select Language Input Box
+LanguageOut.grid(row=IMAGE_ROW+16, column=4, columnspan=2) #Places input box on grid
+Button(main_window, text="Update Language", command=perform_translation, background=BUTTN_COLOR, width=20).grid(row=18,column=4, columnspan=2)
+Button(main_window, text="Revert Language", command=PlaceHolder, background=BUTTN_COLOR, width=20).grid(row=19,column=4, columnspan=2)
+lang_list = Listbox(main_window, height=6) # creates list box within new window
+lang_list.grid(row=17, column=5, columnspan=2, rowspan=3, padx=0, pady=0) # places listbox on grid
+# places every language within list box
+for name, code in languages: 
+    lang_list.insert(END, f"{name}")
 
+#Run main window
 main_window.mainloop()
